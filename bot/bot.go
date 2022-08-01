3fccc30a8b6b711f38f254ec3a54db15be214104
chkatsaros <chris.d.katsaros@gmail.com>
2022-08-01 13:25:27 +0300

    Update config input

--- bot/bot.go
+++ bot/bot.go
@@ -40,7 +40,7 @@ func Start() {
 		return
 	}
 	//If every thing works fine we will be printing this.
-	fmt.Println("Bot is running !")
+	fmt.Println("Bot is running!")
 }
 
 //Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
--- config/config.go
+++ config/config.go
@@ -1,53 +1,40 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var (
 	Token     string
 	BotPrefix string
-
-	config *configStruct
 )
-
-type configStruct struct {
-	Token     string `json : "Token"`
-	BotPrefix string `json : "BotPrefix"`
-}
 
 func ReadConfig() error {
 	fmt.Println("Reading config file...")
-	/* ioutil package's ReadFile method which we read config.json and return it's value we will then
-	store it in file variable and if an error ocurrs it will be stored in err . */
-	file, err := ioutil.ReadFile("./config.json")
 
-	// Handling error and printing it using fmt package's Println function and returning it .
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
+	Token = goDotEnvVariable("token")
+	BotPrefix = goDotEnvVariable("botPrefix")
 
-	// We are here printing value of file variable by explicitly converting it to string .
-
-	fmt.Println(string(file))
-	/* Here we performing a simple task by copying value of file into config variable which we have declared above,
-	and if there any error we are storing it in err . Unmarshal takes second arguments reference remember it. */
-	err = json.Unmarshal(file, &config)
-
-	// Handling error
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	// After storing value in config variable we will access it and storing it in our declared variables.
-	Token = config.Token
-	BotPrefix = config.BotPrefix
+	fmt.Println("==> Token: ", Token)
+	fmt.Println("==> BotPrefix: ", BotPrefix)
 
 	// If there isn't any error we will return nil.
 	return nil
 
+}
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
