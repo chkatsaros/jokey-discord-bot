cc3fe99a05bdfcf321339dde94f0cce417ed35cc
chkatsaros <chris.d.katsaros@gmail.com>
2022-08-01 20:23:41 +0300

    Update ReadConfig

--- config/config.go
+++ config/config.go
@@ -2,10 +2,7 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var (
@@ -16,8 +13,8 @@ var (
 func ReadConfig() error {
 	fmt.Println("Reading config file...")
 
-	Token = goDotEnvVariable("TOKEN")
-	BotPrefix = goDotEnvVariable("BOT_PREFIX")
+	Token = os.Getenv("TOKEN")
+	BotPrefix = os.Getenv("BOT_PREFIX")
 
 	fmt.Println("==> Token: ", Token)
 	fmt.Println("==> BotPrefix: ", BotPrefix)
@@ -25,16 +22,4 @@ func ReadConfig() error {
 	// If there isn't any error we will return nil.
 	return nil
 
-}
-
-func goDotEnvVariable(key string) string {
-
-	// load .env file
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	return os.Getenv(key)
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require github.com/icelain/jokeapi v0.0.0-20220428143244-f71f87248478
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 )
