eede83a0fa21da9f08da22a9607d2e06c8709d2d
chkatsaros <chris.d.katsaros@gmail.com>
2022-08-01 19:38:08 +0300

    Add jokeapi api wrapper dependency, integrate the GOOD joke api

--- bot/bot.go
+++ bot/bot.go
@@ -1,45 +1,15 @@
 package bot
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"jokey-discord-bot/config"
-	"net/http"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/icelain/jokeapi"
 )
 
 var BotId string
-var goBot *discordgo.Session
-
-// var jokeAPI string = "https://v2.jokeapi.dev/joke/"
-var jokeAPI string = "https://api.jokes.one"
-
-type JokeApiResponse struct {
-	Success struct {
-		Total int `json:"total"`
-	}
-	Contents struct {
-		Jokes []struct {
-			Category    string `json:"category"`
-			Title       string `json:"title"`
-			Description string `json:"description"`
-			Background  string `json:"background"`
-			Date        string `json:"date"`
-			Joke        struct {
-				Title  string `json:"title"`
-				Length string `json:"length"`
-				Clean  string `json:"clean"`
-				Racial string `json:"racial"`
-				Date   string `json:"date"`
-				Id     string `json:"id"`
-				Text   string `json:"text"`
-			}
-		}
-	}
-	Copyright string `json:"copyright"`
-}
 
 func Start() {
 
@@ -75,7 +45,7 @@ func Start() {
 	}
 
 	//If every thing works fine we will be printing this.
-	fmt.Println("Bot is alive!")
+	fmt.Println("Jokey is up and running!")
 }
 
 //Definition of messageHandler function it takes two arguments first one is discordgo.Session which is s , second one is discordgo.MessageCreate which is m.
@@ -85,30 +55,33 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	// If we message ping to our bot in our discord it will return us pong .
-	if m.Content == "joke" {
-		res, err := http.Get(jokeAPI + "/jod")
+	if strings.ToLower(m.Content) == "joke" {
 
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+		joke := getRandomJoke()
 
-		body, err := ioutil.ReadAll(res.Body)
+		_, _ = s.ChannelMessageSend(m.ChannelID, joke)
+	}
+}
 
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+func getRandomJoke() string {
+	ctgs := []string{"Programming", "Dark", "Miscellaneous", "Pun", "Spooky"}
 
-		var joke JokeApiResponse
-		if err := json.Unmarshal(body, &joke); err != nil {
-			fmt.Println("Couldn't unmarshal JSON")
-		}
+	api := jokeapi.New()
 
-		_, _ = s.ChannelMessageSend(m.ChannelID, joke.Contents.Jokes[0].Joke.Text)
+	api.Set(jokeapi.Params{Categories: ctgs})
+	res, err := api.Fetch()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
 	}
 
-	// if strings.Contains(m.Content, "joke") {
-	// 	fmt.Println("User needs a joke")
-	// }
+	switch res.JokeType {
+	case "single":
+		return res.Joke[0]
+	case "twopart":
+		return res.Joke[0] + "\n" + res.Joke[1]
+	}
+
+	return ""
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.18
 
 require github.com/bwmarrin/discordgo v0.25.0
 
+require github.com/icelain/jokeapi v0.0.0-20220428143244-f71f87248478
+
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.4.0
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 )
