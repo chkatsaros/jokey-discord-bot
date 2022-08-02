5d77f9da028ebd2f4a87a50845d69b7461d7b517
chkatsaros <chris.d.katsaros@gmail.com>
2022-08-02 13:10:24 +0300

    Try button interaction

--- bot/bot.go
+++ bot/bot.go
@@ -69,41 +69,33 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		var complexMssg discordgo.MessageSend
 
 		complexMssg.Components = []discordgo.MessageComponent{
+			// ActionRow is a container of all buttons within the same row.
 			discordgo.ActionsRow{
 				Components: []discordgo.MessageComponent{
-					discordgo.SelectMenu{
-						// Select menu, as other components, must have a customID, so we set it to this value.
-						CustomID:    "select",
-						Placeholder: "Choose your favorite programming language 👇",
-						Options: []discordgo.SelectMenuOption{
-							{
-								Label: "Go",
-								// As with components, this things must have their own unique "id" to identify which is which.
-								// In this case such id is Value field.
-								Value: "go",
-								Emoji: discordgo.ComponentEmoji{
-									Name: "🦦",
-								},
-								// You can also make it a default option, but in this case we won't.
-								Default:     false,
-								Description: "Go programming language",
-							},
-							{
-								Label: "JS",
-								Value: "js",
-								Emoji: discordgo.ComponentEmoji{
-									Name: "🟨",
-								},
-								Description: "JavaScript programming language",
-							},
-							{
-								Label: "Python",
-								Value: "py",
-								Emoji: discordgo.ComponentEmoji{
-									Name: "🐍",
-								},
-								Description: "Python programming language",
-							},
+					discordgo.Button{
+						// Label is what the user will see on the button.
+						Label: "Yes",
+						// Style provides coloring of the button. There are not so many styles tho.
+						Style: discordgo.SuccessButton,
+						// Disabled allows bot to disable some buttons for users.
+						Disabled: false,
+						// CustomID is a thing telling Discord which data to send when this button will be pressed.
+						CustomID: "fd_yes",
+					},
+					discordgo.Button{
+						Label:    "No",
+						Style:    discordgo.DangerButton,
+						Disabled: false,
+						CustomID: "fd_no",
+					},
+					discordgo.Button{
+						Label:    "I don't know",
+						Style:    discordgo.LinkButton,
+						Disabled: false,
+						// Link buttons don't require CustomID and do not trigger the gateway/HTTP event
+						URL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+						Emoji: discordgo.ComponentEmoji{
+							Name: "🤷",
 						},
 					},
 				},
